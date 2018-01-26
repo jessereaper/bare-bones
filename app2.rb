d969146ffd7d0ad8e1445afd81534ff2837a49e4
jessereaper <gesamel@hotmail.com>
2018-01-26 16:29:43 -0500

    fixed errors in first code

--- app2.rb
+++ app2.rb
@@ -1,29 +1,21 @@
 require 'rack'
 
 app = Proc.new do |env|
+  puts env
   path = env['PATH_INFO'][1..-1]
-filename = if path == ""
-             "index"
-           elsif File.file?("views/#{path}.html")
-             path
-           else
-             "404"
-           end
-page = File.read("views/#{filename}.html")
+  filename = if path == ""
+               "index"
+             elsif File.file?("views/#{path}.html")
+               path
+             else
+               "404"
+             end
+  page = File.read("views/#{filename}.html")
 
-[
-  '200',
-  {'Content-Type' => 'text/html'},
-  [page]
-]
-end
   [
     '200',
     {'Content-Type' => 'text/html'},
-    ["
-      <h1>Hello world!</h1>
-      <p>The time is #{Time.now}</p>
-    "]
+    [page]
   ]
 end
 
