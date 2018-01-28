03454fd4b3fafb89487d3a4c0ab55968b056ca7a
jessereaper <gesamel@hotmail.com>
2018-01-28 16:50:45 -0500

    another syntax error.

--- app2.rb
+++ app2.rb
@@ -20,20 +20,6 @@ require 'rack'
 +  HEREDOC
 +end
 
-app = Proc.new do |env|
-  status = '200'
-  puts env
-  path = env['PATH_INFO'][1..-1]
-  filename = if path == ""
-               "index"
-             elsif File.file?("views/#{path}.html")
-               path
-             else
-               status ="404"
-               "404"
-             end
-  page = File.read("views/#{filename}.html")
-
   [
     status,
     {'Content-Type' => 'text/html'},
