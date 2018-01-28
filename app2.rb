b0d11e1b9d30e82f7d2754cc83aebded1b468304
jessereaper <gesamel@hotmail.com>
2018-01-28 16:47:29 -0500

    added a layout to the pages .

--- app2.rb
+++ app2.rb
@@ -1,5 +1,27 @@
 require 'rack'
 
+require 'rack'
+
++def layout(page)
++  <<~HEREDOC
++    <!DOCTYPE html>
++    <html>
++      <head>
++        <meta charset="utf-8">
++        <meta name="viewport" content="width=device-width, initial-scale=1">
++        <title>The Simplest Ruby App</title>
++        <style type="text/css">
++          body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}
++          h1,h2,h3{line-height:1.2}
++        </style>
++      </head>
++      <body>
++        #{page}
++      </body>
++    </html>
++  HEREDOC
++end
+
 app = Proc.new do |env|
   status = '200'
   puts env
@@ -17,7 +39,7 @@ app = Proc.new do |env|
   [
     status,
     {'Content-Type' => 'text/html'},
-    [page]
+    [layout(page)]
   ]
 end
 
