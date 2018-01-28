b1d384858f270d3b121d2f217d204c6935da3dd8
jessereaper <gesamel@hotmail.com>
2018-01-28 17:32:04 -0500

    changed alot hope it works now

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source "https://rubygems.org" #this is where ur browser gets a gem file
 ruby "2.5.0" # make sure this matches ur current version on ruby
 
-gem "rack" #this is the gem im using
+#gem "rack" #this is the gem im using
+gem 'cuba'
+
+gem 'tilt'
--- app2.rb
+++ app2.rb
@@ -1,3 +1,4 @@
+=begin
 class App
   def layout(page)
     <<~HEREDOC
@@ -38,5 +39,26 @@ class App
       {'Content-Type' => 'text/html'},
       [layout(page)]
     ]
+  end
+end
+=end
+require "cuba"
+require "cuba/render"
+require "erb"
+
+Cuba.plugin Cuba::Render
+
+Cuba.define do
+  on "about" do
+    res.write view("about")
+  end
+
+  on root do
+    res.write view("index")
+  end
+
+  def not_found
+    super
+    res.write view("404")
   end
 end
