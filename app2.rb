e13a9be1f647b400c8badde19bd1185b75791987
jessereaper <gesamel@hotmail.com>
2018-01-28 16:38:57 -0500

    changed the way status was being used on the app.rb .

--- app2.rb
+++ app2.rb
@@ -1,6 +1,7 @@
 require 'rack'
 
 app = Proc.new do |env|
+  status = '200'
   puts env
   path = env['PATH_INFO'][1..-1]
   filename = if path == ""
@@ -8,12 +9,13 @@ app = Proc.new do |env|
              elsif File.file?("views/#{path}.html")
                path
              else
+               status ="404"
                "404"
              end
   page = File.read("views/#{filename}.html")
 
   [
-    '200',
+    status,
     {'Content-Type' => 'text/html'},
     [page]
   ]
